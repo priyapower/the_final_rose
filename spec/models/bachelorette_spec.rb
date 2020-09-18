6e807354549ffed716bf2b3ca55f2dec67a41c32
Priya Power <priyajag72@gmail.com>
2020-09-18 16:36:12 -0600

    Update bachelorette spec for more robust testing

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bachelorette, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :season_number}
+  end
+
   describe 'relationships' do
     it {should have_many :contestants}
   end
