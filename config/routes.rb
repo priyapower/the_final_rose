1743dc20194ec4f7197ce4525d0feeaaba5f0d26
Priya Power <priyajag72@gmail.com>
2020-09-18 18:08:29 -0600

    Add route for contestants show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :bachelorettes, only: :show do
     get "/contestants", to: "bachelorettes/contestants#index"
   end
+
+  resources :contestants, only: :show
 end
