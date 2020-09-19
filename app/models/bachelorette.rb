8fdbf3a149192b218205bba8f0e649852f368661
Priya Power <priyajag72@gmail.com>
2020-09-18 18:52:46 -0600

    Add instance method average contestants age

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -4,4 +4,8 @@ class Bachelorette <ApplicationRecord
   validates_presence_of :name
   validates_presence_of :season_number
   validates_presence_of :season_description
+
+  def average_contestants_age
+    contestants.average(:age)
+  end
 end
