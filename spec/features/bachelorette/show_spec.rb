208ed36ac9edbd0c962fd6cc6e97af30e18efeb2
Priya Power <priyajag72@gmail.com>
2020-09-18 18:52:02 -0600

    Add test for US5 - bachelorette contestants average age

--- spec/features/bachelorette/show_spec.rb
+++ spec/features/bachelorette/show_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Bachelorette Show Page", type: :feature do
       @bachelorette_1 = Bachelorette.create!(name: "Daniel", season_number: 3, season_description: "An amazing human that likes to give magic to all")
       @bachelorette_2 = Bachelorette.create!(name: "Drew", season_number: 1, season_description: "The best drawer of sting rays")
       @bachelorette_3 = Bachelorette.create!(name: "Corey", season_number: 7, season_description: "You Love To See It")
-      @contestant_1 = @bachelorette_1.contestants.create!(name: "Leah", age: 21, hometown: "Denver")
-      @contestant_2 = @bachelorette_1.contestants.create!(name: "Arique", age: 19, hometown: "San Antonio")
+      @contestant_1 = @bachelorette_1.contestants.create!(name: "Leah", age: 19, hometown: "Denver")
+      @contestant_2 = @bachelorette_1.contestants.create!(name: "Arique", age: 25, hometown: "San Antonio")
       @contestant_3 = @bachelorette_2.contestants.create!(name: "Saryn", age: 22, hometown: "Phoenix")
       @contestant_4 = @bachelorette_2.contestants.create!(name: "Judith", age: 20, hometown: "New York")
       @contestant_5 = @bachelorette_3.contestants.create!(name: "Dani", age: 23, hometown: "San Diego")
@@ -20,6 +20,11 @@ RSpec.describe "Bachelorette Show Page", type: :feature do
       expect(page).to have_content(@bachelorette_1.name)
       expect(page).to have_content(@bachelorette_1.season_number)
       expect(page).to have_content(@bachelorette_1.season_description)
+    end
+
+    it "can see average age of all the bachelorette contestants" do
+      visit "/bachelorettes/#{@bachelorette_1.id}"
+      expect(page).to have_content(@bachelorette_1.average_contestants_age)
     end
 
   end
